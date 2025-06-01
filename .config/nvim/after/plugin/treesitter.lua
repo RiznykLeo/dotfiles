713818a504eedc67378863e95651815b80551d1c
RiznykLeo <riznyk.leo@gmail.com>
2025-06-02 07:57:07 +0900

    fix: nvim auto close html tags

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -22,11 +22,4 @@ require("nvim-treesitter.configs").setup({
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
-
-	autotag = { --For closing html/jsx tags
-		enable = true,
-		enable_rename = true,
-		enable_close = true,
-		enable_close_on_slash = true,
-	},
 })
